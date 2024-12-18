a17881a76d2a34a9ad47391ebf1825ea930758b6
koheimizuno <skygentleman626@gmail.com>
2024-12-18 21:56:11 +0900

    removed isRequired

--- Spreadsheet/A【営業】21スケジュール管理シート/getFromSheetToKintone.js
+++ Spreadsheet/A【営業】21スケジュール管理シート/getFromSheetToKintone.js
@@ -4,14 +4,6 @@ const appAccess = {
     apiToken: "hB4jVlUAnoKwXLJjZJf3C9vp56CfYJycJIxpOg2j",
     appId: "6",
   },
-  businessProcessApp: {
-    apiToken: "aUoQGeBrMrU6pgIMDOIosia5P7yqkLvZbAhgHMoX",
-    appId: "21",
-  },
-  contactManApp: {
-    apiToken: "u33RgrJmimilhnpniQfASOdyRKfgDfrSBMKfcdDR",
-    appId: "22",
-  },
 };
 
 const mainSheet =
@@ -24,62 +16,15 @@ const currentDataRowCount = mainSheet.getLastRow() - 4;
 
 // 顧客管理
 const clientManColumns = [
-  { colId: "G", isDateField: false, isRequired: true },
-  { colId: "T", isDateField: false, isRequired: true },
-  { colId: "U", isDateField: false, isRequired: true },
-  { colId: "V", isDateField: false, isRequired: true },
-  { colId: "W", isDateField: false, isRequired: true },
-];
-
-// 営業進捗
-const businessProcessColumns = [
-  { colId: "A", isDateField: false, isRequired: true },
-  { colId: "B", isDateField: false, isRequired: true },
-  { colId: "C", isDateField: true, isRequired: true },
-  { colId: "E", isDateField: false, isRequired: true },
-  { colId: "H", isDateField: false, isRequired: false },
-  { colId: "I", isDateField: false, isRequired: true },
-  { colId: "J", isDateField: false, isRequired: true },
-  { colId: "K", isDateField: false, isRequired: true },
-  { colId: "L", isDateField: false, isRequired: true },
-  { colId: "M", isDateField: false, isRequired: false },
-  { colId: "N", isDateField: false, isRequired: false },
-  { colId: "O", isDateField: false, isRequired: true },
-  { colId: "P", isDateField: true, isRequired: false },
-  { colId: "Q", isDateField: false, isRequired: false },
-  { colId: "R", isDateField: false, isRequired: false },
-  { colId: "S", isDateField: false, isRequired: false },
-  { colId: "X", isDateField: false, isRequired: true },
-  { colId: "Y", isDateField: true, isRequired: false },
-  { colId: "AL", isDateField: false, isRequired: false },
-];
-
-// 問合せ管理
-const contactManColumns = [
-  { colId: "A", isDateField: false, isRequired: true },
-  { colId: "AM", isDateField: false, isRequired: false },
-  { colId: "AI", isDateField: true, isRequired: false },
-  { colId: "AJ", isDateField: true, isRequired: false },
-  { colId: "AK", isDateField: true, isRequired: false },
-  { colId: "Z", isDateField: false, isRequired: false },
-  { colId: "AA", isDateField: true, isRequired: false },
-  { colId: "AB", isDateField: false, isRequired: false },
-  { colId: "AC", isDateField: true, isRequired: false },
-  { colId: "AD", isDateField: false, isRequired: false },
-  { colId: "AE", isDateField: true, isRequired: false },
-  { colId: "AF", isDateField: false, isRequired: false },
-  { colId: "AG", isDateField: true, isRequired: false },
-  { colId: "AH", isDateField: true, isRequired: false },
+  { colId: "G", isDateField: false },
+  { colId: "T", isDateField: false },
+  { colId: "U", isDateField: false },
+  { colId: "V", isDateField: false },
+  { colId: "W", isDateField: false },
 ];
 
 function addRecordsToKintone() {
   let clientManAppDataList = clientManColumns.map((item) =>
-    getColumnListById(item)
-  );
-  let businessProcessDataList = businessProcessColumns.map((item) =>
-    getColumnListById(item)
-  );
-  let contactManAppDataList = contactManColumns.map((item) =>
     getColumnListById(item)
   );
 
@@ -89,18 +34,4 @@ function addRecordsToKintone() {
     appId: appAccess.clientManApp.appId,
     uniqueKey: "・代表者様氏名_フルネーム",
   });
-
-  // addMultipleRecords({
-  //   dataList: businessProcessDataList,
-  //   apiToken: `${appAccess.businessProcessApp.apiToken}, ${appAccess.clientManApp.apiToken}`,
-  //   appId: appAccess.businessProcessApp.appId,
-  //   uniqueKey: "顧客番号",
-  // });
-
-  // addMultipleRecords({
-  //   dataList: contactManAppDataList,
-  //   apiToken: `${appAccess.contactManApp.apiToken}, ${appAccess.clientManApp.apiToken}`,
-  //   appId: appAccess.contactManApp.appId,
-  //   uniqueKey: "顧客番号",
-  // });
 }
